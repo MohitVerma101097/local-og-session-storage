6d7f32c6ee80a550b3c8ac12efa03b5ca1bce19d
Your Name <you@example.com>
2024-03-03 17:36:51 +0100

    fix

--- script2.js
+++ script2.js
@@ -1,7 +1,23 @@
+
+const activityContainer = document.querySelector("#activity-container");
+const savedActivitiesHeading = document.querySelector("h3");
+const savedActivitiesContainer = document.querySelector("#saved-activities-container");
+
 const fetchActivty = async() =>{
     const request = await fetch("http://www.boredapi.com/api/activity/")
-    const result = request.json()
-    console.log(result)
+    const result = await request.json()
+    return result
 }
 
-fetchActivty()
+const fetchAndSave = async() =>{
+    try {
+        const activity = await fetchActivty()
+        showActivity(activity)
+    } catch (error) {
+        console.error("klarte ikke å hente..", error)
+    }
+}
+
+const showActivity = (activity) => {
+
+} 
